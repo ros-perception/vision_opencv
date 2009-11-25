0d13d6222c6bc36c4e96260838e82e850161c400
mihelich <mihelich@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-11-25 01:53:54 +0000

    image_geometry (C++): Unit test for projecting points uv <-> xyz.
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/vision_opencv/trunk@25892 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- image_geometry/test/utest.cpp
+++ image_geometry/test/utest.cpp
@@ -34,7 +34,7 @@ protected:
   image_geometry::PinholeCameraModel model_;
 };
 
-TEST_F(PinholeTest, AccessorsCorrect)
+TEST_F(PinholeTest, accessorsCorrect)
 {
   EXPECT_EQ((unsigned)480, model_.height());
   EXPECT_EQ((unsigned)640, model_.width());
@@ -45,6 +45,42 @@ TEST_F(PinholeTest, AccessorsCorrect)
   EXPECT_EQ(cam_info_.P[6], model_.cy());
 }
 
+TEST_F(PinholeTest, projectPoint)
+{
+  // Spot test an arbitrary point.
+  {
+    CvPoint2D64f uv = {100, 100};
+    CvPoint3D64f xyz;
+    model_.projectPixelTo3dRay(uv, xyz);
+    EXPECT_DOUBLE_EQ(-0.501371686835080932, xyz.x);
+    EXPECT_DOUBLE_EQ(-0.333142973423770972, xyz.y);
+    EXPECT_DOUBLE_EQ(0.798525009563579191, xyz.z);
+  }
+
+  // Principal point should project straight out.
+  {
+    CvPoint2D64f uv = {model_.cx(), model_.cy()};
+    CvPoint3D64f xyz;
+    model_.projectPixelTo3dRay(uv, xyz);
+    EXPECT_DOUBLE_EQ(0.0, xyz.x);
+    EXPECT_DOUBLE_EQ(0.0, xyz.y);
+    EXPECT_DOUBLE_EQ(1.0, xyz.z);
+  }
+  
+  // Check projecting to 3d and back over entire image is accurate.
+  const size_t step = 10;
+  for (size_t row = 0; row <= cam_info_.height; row += step) {
+    for (size_t col = 0; col <= cam_info_.width; col += step) {
+      CvPoint2D64f uv = {row, col}, uv_back;
+      CvPoint3D64f xyz;
+      model_.projectPixelTo3dRay(uv, xyz);
+      model_.project3dToPixel(xyz, uv_back);
+      // Measured max error at 1.13687e-13
+      EXPECT_NEAR(uv.x, uv_back.x, 1.14e-13) << "at (" << row << ", " << col << ")";
+      EXPECT_NEAR(uv.y, uv_back.y, 1.14e-13) << "at (" << row << ", " << col << ")";
+    }
+  }
+}
 
 int main(int argc, char** argv)
 {

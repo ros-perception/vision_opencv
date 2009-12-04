64d9da9c66cb3fdf4f922db6dc9cae64d9c52076
mihelich <mihelich@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-12-04 04:00:48 +0000

    image_geometry: Added some C++ docs.
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/vision_opencv/trunk@26036 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- image_geometry/include/image_geometry/pinhole_camera_model.h
+++ image_geometry/include/image_geometry/pinhole_camera_model.h
@@ -6,46 +6,123 @@
 
 namespace image_geometry {
 
+/**
+ * \brief Simplifies interpreting images geometrically using the parameters from
+ * sensor_msgs/CameraInfo.
+ */
 class PinholeCameraModel
 {
 public:
+  
   PinholeCameraModel();
 
   PinholeCameraModel(const PinholeCameraModel& other);
 
-  // Update in image/info callback
+  /**
+   * \brief Set the camera parameters from the sensor_msgs/CameraInfo message.
+   */
   void fromCameraInfo(const sensor_msgs::CameraInfo& msg);
+
+  /**
+   * \brief Set the camera parameters from the sensor_msgs/CameraInfo message.
+   */
   void fromCameraInfo(const sensor_msgs::CameraInfoConstPtr& msg);
 
-  // Projection functions, 3d points are in tfFrame()
+  /**
+   * \brief Get the name of the camera coordinate frame in tf.
+   */
   std::string tfFrame() const;
-  
+
+  /**
+   * \brief Project a 3d point to rectified pixel coordinates.
+   *
+   * This is the inverse of projectPixelTo3dRay().
+   *
+   * \param xyz 3d point in the camera coordinate frame
+   * \param[out] uv_rect Rectified pixel coordinates
+   */
   void project3dToPixel(const cv::Point3d& xyz, cv::Point2d& uv_rect) const;
 
+  /**
+   * \brief Project a rectified pixel to a 3d ray.
+   *
+   * Returns the unit vector in the camera coordinate frame in the direction of rectified
+   * pixel (u,v) in the image plane. This is the inverse of project3dToPixel().
+   *
+   * \param uv_rect Rectified pixel coordinates
+   * \param[out] ray 3d ray passing through (u,v).
+   */
   void projectPixelTo3dRay(const cv::Point2d& uv_rect, cv::Point3d& ray) const;
 
-  // Rectification
+  /**
+   * \brief Rectify a raw camera image.
+   */
   void rectifyImage(const cv::Mat& raw, cv::Mat& rectified,
                     int interpolation = CV_INTER_LINEAR) const;
 
+  /**
+   * \brief Apply camera distortion to a rectified image.
+   */
   void unrectifyImage(const cv::Mat& rectified, cv::Mat& raw) const;
 
+  /**
+   * \brief Compute the rectified image coordinates of a pixel in the raw image.
+   */
   void rectifyPoint(const cv::Point2d& uv_raw, cv::Point2d& uv_rect) const;
 
+  /**
+   * \brief Compute the raw image coordinates of a pixel in the rectified image.
+   */
   void unrectifyPoint(const cv::Point2d& uv_rect, cv::Point2d& uv_raw) const;
 
-  // Arguments for OpenCV functions
-  const cv::Mat_<double>& intrinsicMatrix() const;  // K, also called camera_matrix in cv docs
-  const cv::Mat_<double>& distortionCoeffs() const; // D, 1x4 or 1x5?
-  const cv::Mat_<double>& rotationMatrix() const;   // R, rectificationMatrix?
-  const cv::Mat_<double>& projectionMatrix() const; // P, 3x4
+  /**
+   * \brief Returns the original camera matrix.
+   */
+  const cv::Mat_<double>& intrinsicMatrix() const;
 
-  // Accessors for specific parameters
+  /**
+   * \brief Returns the distortion coefficients.
+   */
+  const cv::Mat_<double>& distortionCoeffs() const;
+
+  /**
+   * \brief Returns the rotation matrix.
+   */
+  const cv::Mat_<double>& rotationMatrix() const;
+
+  /**
+   * \brief Returns the projection matrix.
+   */
+  const cv::Mat_<double>& projectionMatrix() const;
+
+  /**
+   * \brief Returns the focal length (pixels) in x direction of the rectified image.
+   */
   double fx() const;
+
+  /**
+   * \brief Returns the focal length (pixels) in y direction of the rectified image.
+   */
   double fy() const;
+
+  /**
+   * \brief Returns the x coordinate of the optical center.
+   */
   double cx() const;
+
+  /**
+   * \brief Returns the y coordinate of the optical center.
+   */
   double cy() const;
+
+  /**
+   * \brief Returns the image height.
+   */
   uint32_t height() const;
+
+  /**
+   * \brief Returns the image width.
+   */
   uint32_t width() const;
 
 private:

7ed86430fb481998de93ff6888ecf9ddd6094771
mihelich <mihelich@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-12-04 02:26:55 +0000

    image_geometry: Minor cleanup of StereoCameraModel, added it to build. Put in copy constructors.

--- image_geometry/include/image_geometry/stereo_camera_model.h
+++ image_geometry/include/image_geometry/stereo_camera_model.h
@@ -23,41 +23,39 @@ public:
   // Projection functions, 3d points are in tfFrame()
   std::string tfFrame() const; // Should be same for both cameras
 
-  void project3dToPixel(const Point3d& xyz, Point2d& left_uv_rect) const;
-  void project3dToPixel(const Point3d& xyz, Point2d& left_uv_rect, Point2d& right_uv_rect) const;
-
-  void projectPixelTo3d(const Point2d& left_uv_rect, float disparity, Point3d& xyz) const;
+  void projectDisparityTo3d(const cv::Point2d& left_uv_rect, float disparity, cv::Point3d& xyz) const;
 
   /// @todo Function for projecting disparity image to point cloud
 
   // Arguments for OpenCV functions
-  const CvMat* reprojectionMatrix() const; // Q, 4x4
+  const cv::Mat_<double>& reprojectionMatrix() const; // Q, 4x4
 
   // Accessors for specific parameters
   double baseline() const; //Tx
 
-  /// @todo Maybe more convenience functions from CvStereoCamModel in stereo_util
+  /// @todo Maybe more convenience functions from CvStereoCamModel in stereo_util, like getDelta_.
 
 private:
-  PinholeCameraModel left_, right_;
-  double Q_buf_[16];
-  CvMat Q_;
   bool initialized_;
+  PinholeCameraModel left_, right_;
+  cv::Mat_<double> Q_;
+
+  void updateQ();
 };
 
 
 /* Trivial inline functions */
-inline const StereoCameraModel::PinholeCameraModel& left() const  { return left_; }
-inline const StereoCameraModel::PinholeCameraModel& right() const { return right_; }
+inline const PinholeCameraModel& StereoCameraModel::left() const  { return left_; }
+inline const PinholeCameraModel& StereoCameraModel::right() const { return right_; }
 
 inline std::string StereoCameraModel::tfFrame() const { return left_.tfFrame(); }
 
-inline const CvMat* StereoCameraModel::reprojectionMatrix() const { return &Q_; }
+inline const cv::Mat_<double>& StereoCameraModel::reprojectionMatrix() const { return Q_; }
 
 inline double StereoCameraModel::baseline() const
 {
-  /// @todo Assuming horizontal baseline ok?
-  return -right_.projectionMatrix()->data.db[3] / right_.fx();
+  /// @todo Currently assuming horizontal baseline
+  return -right_.projectionMatrix()(0,3) / right_.fx();
 }
 
 } //namespace image_geometry

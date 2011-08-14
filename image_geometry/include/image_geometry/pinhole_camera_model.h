f2791476930f2bd247a42645e5ab7b24ad455f55
ethanrublee <ethanrublee@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2011-08-14 17:50:19 +0000

    Adding a few missing headers so that client code may compile against pinhole camera model.

--- image_geometry/include/image_geometry/pinhole_camera_model.h
+++ image_geometry/include/image_geometry/pinhole_camera_model.h
@@ -3,6 +3,8 @@
 
 #include <sensor_msgs/CameraInfo.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <stdexcept>
 
 namespace image_geometry {

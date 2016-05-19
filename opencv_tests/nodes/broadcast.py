7818b7b2fc7c47cef84b4a42ccdb70a8cdc2e344
talregev <tal.regev@gmail.com>
2016-05-19 22:39:56 +0300

    Support compressed Images messages in python for indigo - Add cv2_to_comprssed_imgmsg: Convert from cv2 image to compressed image ros msg. - Add comprssed_imgmsg_to_cv2:   Convert the compress message to a new image. - Add compressed image tests. - Add time to msgs (compressed and regular).
    add enumerants test for compressed image.
    merge the compressed tests with the regular ones.
    better comment explanation. I will squash this commit.
    
    Fix indentation
    fix typo mistage: from .imgmsg_to_compressed_cv2 to .compressed_imgmsg_to_cv2.
    
    remove cv2.CV_8UC1
    remove rospy and time depndency.
    
    change from IMREAD_COLOR to IMREAD_ANYCOLOR.
    
    - make indentaion of 4.
    - remove space trailer.
    - remove space from empty lines.
    - another set of for loops, it will make things easier to track. In that new set,  just have the number of channels in ([],1,3,4) (ignore two for jpg). from: https://github.com/ros-perception/vision_opencv/pull/132#discussion_r66721943
    - keep the OpenCV error message. from: https://github.com/ros-perception/vision_opencv/pull/132#discussion_r66721013
    
    add debug print for test.
    
    add case for 4 channels in test.
    
    remove 4 channels case from compressed test.
    
    add debug print for test.
    
    change typo of format.
    
    fix typo in format. change from dip to dib.
    change to IMREAD_ANYCOLOR as python code. (as it should).
    
    rename TIFF to tiff
    
    Sperate the tests one for regular images and one for compressed.
    
    update comment

--- opencv_tests/nodes/broadcast.py
+++ opencv_tests/nodes/broadcast.py
@@ -2,6 +2,7 @@
 # Software License Agreement (BSD License)
 #
 # Copyright (c) 2008, Willow Garage, Inc.
+# Copyright (c) 2016, Tal Regev.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -40,32 +41,38 @@ import cv2
 import sensor_msgs.msg
 from cv_bridge import CvBridge
 
-class source:
 
-  def __init__(self, topic, filenames):
-    self.pub = rospy.Publisher(topic, sensor_msgs.msg.Image)
-    self.filenames = filenames
+# Send each image by iterate it from given array of files names  to a given topic,
+# as a regular and compressed ROS Images msgs.
+class Source:
 
-  def spin(self):
-    time.sleep(1.0)
-    cvb = CvBridge()
-    while not rospy.core.is_shutdown():
-      cvim = cv2.imload(self.filenames[0])
-      self.pub.publish(cvb.cv2_to_imgmsg(cvim))
-      self.filenames = self.filenames[1:] + [self.filenames[0]]
-      time.sleep(1)
+    def __init__(self, topic, filenames):
+        self.pub            = rospy.Publisher(topic, sensor_msgs.msg.Image)
+        self.pub_compressed = rospy.Publisher(topic + "/compressed", sensor_msgs.msg.CompressedImage)
+        self.filenames      = filenames
+
+    def spin(self):
+        time.sleep(1.0)
+        cvb = CvBridge()
+        while not rospy.core.is_shutdown():
+            cvim = cv2.imload(self.filenames[0])
+            self.pub.publish(cvb.cv2_to_imgmsg(cvim))
+            self.pub_compressed.publish(cvb.cv2_to_compressed_imgmsg(cvim))
+            self.filenames = self.filenames[1:] + [self.filenames[0]]
+            time.sleep(1)
+
 
 def main(args):
-  s = source(args[1], args[2:])
-  rospy.init_node('source')
-  try:
-    s.spin()
-    rospy.spin()
-    outcome = 'test completed'
-  except KeyboardInterrupt:
-    print "shutting down"
-    outcome = 'keyboard interrupt'
-  rospy.core.signal_shutdown(outcome)
+    s = Source(args[1], args[2:])
+    rospy.init_node('Source')
+    try:
+        s.spin()
+        rospy.spin()
+        outcome = 'test completed'
+    except KeyboardInterrupt:
+        print "shutting down"
+        outcome = 'keyboard interrupt'
+    rospy.core.signal_shutdown(outcome)
 
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)

441f27b33ee5caae2f11647036572c60a8473667
talregev <tal.regev@gmail.com>
2016-05-19 22:39:56 +0300

    Support compressed Images messages in python for indigo - Add cv2_to_comprssed_imgmsg: Convert from cv2 image to compressed image ros msg. - Add comprssed_imgmsg_to_cv2:   Convert the compress message to a new image. - Add compressed image tests. - Add time to msgs (compressed and regular).
    add enumerants test for compressed image.
    merge the compressed tests with the regular ones.
    better comment explanation. I will squash this commit.
    
    Fix indentation
    fix typo mistage: from .imgmsg_to_compressed_cv2 to .compressed_imgmsg_to_cv2.
    
    remove cv2.CV_8UC1
    remove rospy and time depndency.
    
    change from IMREAD_COLOR to IMREAD_ANYCOLOR.
    
    - make indentaion of 4.
    - remove space trailer.
    - remove space from empty lines.
    - another set of for loops, it will make things easier to track. In that new set,  just have the number of channels in ([],1,3,4) (ignore two for jpg). from: https://github.com/ros-perception/vision_opencv/pull/132#discussion_r66721943
    - keep the OpenCV error message. from: https://github.com/ros-perception/vision_opencv/pull/132#discussion_r66721013
    
    add debug print for test.
    
    add case for 4 channels in test.
    
    remove 4 channels case from compressed test.
    
    add debug print for test.
    
    change typo of format.
    
    fix typo in format. change from dip to dib.
    change to IMREAD_ANYCOLOR as python code. (as it should).
    
    rename TIFF to tiff
    
    Sperate the tests one for regular images and one for compressed.
    
    update comment

--- cv_bridge/test/enumerants.py
+++ cv_bridge/test/enumerants.py
@@ -6,6 +6,7 @@ import sensor_msgs.msg
 
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class TestEnumerants(unittest.TestCase):
 
     def test_enumerants_cv2(self):
@@ -31,7 +32,7 @@ class TestEnumerants(unittest.TestCase):
     def test_encode_decode_cv2(self):
         import cv2
         import numpy as np
-        fmts = [ cv2.CV_8U, cv2.CV_8S, cv2.CV_16U, cv2.CV_16S, cv2.CV_32S, cv2.CV_32F, cv2.CV_64F ]
+        fmts = [cv2.CV_8U, cv2.CV_8S, cv2.CV_16U, cv2.CV_16S, cv2.CV_32S, cv2.CV_32F, cv2.CV_64F]
 
         cvb_en = CvBridge()
         cvb_de = CvBridge()
@@ -39,13 +40,41 @@ class TestEnumerants(unittest.TestCase):
         for w in range(100, 800, 100):
             for h in range(100, 800, 100):
                 for f in fmts:
-                    for channels in ([],1,2,3,4):
+                    for channels in ([], 1, 2, 3, 4):
                         if channels == []:
                             original = np.uint8(np.random.randint(0, 255, size=(h, w)))
                         else:
                             original = np.uint8(np.random.randint(0, 255, size=(h, w, channels)))
                         rosmsg = cvb_en.cv2_to_imgmsg(original)
                         newimg = cvb_de.imgmsg_to_cv2(rosmsg)
+
+                        self.assert_(original.dtype == newimg.dtype)
+                        if channels == 1:
+                            # in that case, a gray image has a shape of size 2
+                            self.assert_(original.shape[:2] == newimg.shape[:2])
+                        else:
+                            self.assert_(original.shape == newimg.shape)
+                        self.assert_(len(original.tostring()) == len(newimg.tostring()))
+
+    def test_encode_decode_cv2_compressed(self):
+        import numpy as np
+        # from: http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html#Mat imread(const string& filename, int flags)
+        formats = ["jpg", "jpeg", "jpe", "png", "bmp", "dib", "ppm", "pgm", "pbm",
+                   "jp2", "sr", "ras", "tif", "tiff"]  # this formats rviz is not support
+
+        cvb_en = CvBridge()
+        cvb_de = CvBridge()
+
+        for w in range(100, 800, 100):
+            for h in range(100, 800, 100):
+                for f in formats:
+                    for channels in ([], 1, 3):
+                        if channels == []:
+                            original = np.uint8(np.random.randint(0, 255, size=(h, w)))
+                        else:
+                            original = np.uint8(np.random.randint(0, 255, size=(h, w, channels)))
+                        compress_rosmsg = cvb_en.cv2_to_compressed_imgmsg(original, f)
+                        newimg          = cvb_de.compressed_imgmsg_to_cv2(compress_rosmsg)
                         self.assert_(original.dtype == newimg.dtype)
                         if channels == 1:
                             # in that case, a gray image has a shape of size 2
@@ -64,8 +93,8 @@ class TestEnumerants(unittest.TestCase):
 
     def test_mono16_cv2(self):
         import numpy as np
-        br = CvBridge()
-        im = np.uint8(np.random.randint(0, 255, size=(480, 640, 3)))
+        br  = CvBridge()
+        im  = np.uint8(np.random.randint(0, 255, size=(480, 640, 3)))
         msg = br.cv2_to_imgmsg(im)
 
 if __name__ == '__main__':

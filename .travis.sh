11f9ca54ee529d80641b21af393906ca1c4006eb
Vincent Rabaud <vincent.rabaud@gmail.com>
2016-09-24 20:43:52 +0200

    add quiet to apt-get Travis

--- .travis.sh
+++ .travis.sh
@@ -32,7 +32,7 @@ sudo sh -c "echo \"deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_rele
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
 # Install ROS
-sudo apt-get install -y python-catkin-pkg python-catkin-tools python-rosdep python-wstool ros-$ROS_DISTRO-catkin
+sudo apt-get install -qq -y python-catkin-pkg python-catkin-tools python-rosdep python-wstool ros-$ROS_DISTRO-catkin
 source /opt/ros/$ROS_DISTRO/setup.bash
 # Setup for rosdep
 sudo rosdep init

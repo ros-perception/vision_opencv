0bbebb73bea65eca856e732b919439853a04986b
Sean Yen <seanyen@microsoft.com>
2019-07-10 11:23:02 -0700

    remove path splash separator from 'package_dir' (#267)

--- cv_bridge/setup.py
+++ cv_bridge/setup.py
@@ -5,6 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup()
 
 d['packages'] = ['cv_bridge']
-d['package_dir'] = {'' : 'python/'}
+d['package_dir'] = {'' : 'python'}
 
 setup(**d)

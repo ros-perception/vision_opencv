0555d61409f48a04ad873aa105a27c43135e229f
Kenji Brameld <kenjibrameld@gmail.com>
2022-07-31 21:32:47 +0000

    fix mistake in deprecated warning
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- image_geometry/include/image_geometry/stereo_camera_model.h
+++ image_geometry/include/image_geometry/stereo_camera_model.h
@@ -3,7 +3,7 @@
 
 // This file is deprecated as of I-turtle and can be completely removed in J-turtle.
 
-#warning This header is obsolete, please include image_geometry/pinhole_camera_model.hpp instead
+#warning This header is obsolete, please include image_geometry/stereo_camera_model.hpp instead
 
 #include <image_geometry/stereo_camera_model.hpp>
 

864186b11619bd1c8e5e4ad5862e5a787a32819c
Kenji Brameld <kenjibrameld@gmail.com>
2022-07-31 20:39:07 +0000

    add deprecated .h files
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

---
+++ image_geometry/include/image_geometry/pinhole_camera_model.h
@@ -0,0 +1,8 @@
+#ifndef IMAGE_GEOMETRY__PINHOLE_CAMERA_MODEL_H
+#define IMAGE_GEOMETRY__PINHOLE_CAMERA_MODEL_H
+
+#warning This header is obsolete, please include image_geometry/pinhole_camera_model.hpp instead
+
+#include <image_geometry/pinhole_camera_model.hpp>
+
+#endif

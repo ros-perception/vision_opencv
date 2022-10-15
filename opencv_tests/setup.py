9ef55ae6c001e3f9912cc623aea189edda8f1c59
Kenji Brameld <kenjibrameld@gmail.com>
2022-10-15 22:13:08 +0900

    3.1.3

--- opencv_tests/setup.py
+++ opencv_tests/setup.py
@@ -5,7 +5,7 @@ package_name = 'opencv_tests'
 
 setup(
     name=package_name,
-    version='3.1.2',
+    version='3.1.3',
     packages=find_packages(exclude=['launch']),
     data_files=[
         ('share/ament_index/resource_index/packages',

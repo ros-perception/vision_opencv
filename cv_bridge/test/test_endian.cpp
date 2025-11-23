493b01e25922ec161ebfcf5ed7d25712ca440785
Michael Carlstrom <rmc@carlstrom.com>
2025-11-22 21:03:08 -0800

    remove boost/endian
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- cv_bridge/test/test_endian.cpp
+++ cv_bridge/test/test_endian.cpp
@@ -1,12 +1,10 @@
-#include <boost/endian/conversion.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 #include <gtest/gtest.h>
 #include <memory>
+#include <rcpputils/endian.hpp>
 
 TEST(CvBridgeTest, endianness)
 {
-  using namespace boost::endian;
-
   // Create an image of the type opposite to the platform
   sensor_msgs::msg::Image msg;
   msg.height = 1;
@@ -15,17 +13,31 @@ TEST(CvBridgeTest, endianness)
   msg.step = 8;
 
   msg.data.resize(msg.step);
-  int32_t * data = reinterpret_cast<int32_t *>(&msg.data[0]);
+  uint8_t * raw = msg.data.data();
 
   // Write 1 and 2 in order, but with an endianness opposite to the platform
-  if (order::native == order::little) {
+  if (rcpputils::endian::native == rcpputils::endian::little) {
     msg.is_bigendian = true;
-    *(data++) = native_to_big(static_cast<int32_t>(1));
-    *data = native_to_big(static_cast<int32_t>(2));
+    raw[0] = 0x00;
+    raw[1] = 0x00;
+    raw[2] = 0x00;
+    raw[3] = 0x01;
+
+    raw[4] = 0x00;
+    raw[5] = 0x00;
+    raw[6] = 0x00;
+    raw[7] = 0x02;
   } else {
     msg.is_bigendian = false;
-    *(data++) = native_to_little(static_cast<int32_t>(1));
-    *data = native_to_little(static_cast<int32_t>(2));
+    raw[0] = 0x01;
+    raw[1] = 0x00;
+    raw[2] = 0x00;
+    raw[3] = 0x00;
+
+    raw[4] = 0x02;
+    raw[5] = 0x00;
+    raw[6] = 0x00;
+    raw[7] = 0x00;
   }
 
   // Make sure the values are still the same
